1fca6748ad4841b9bf7472c536cc65632743c27c
Sebastian Moser <sebastian@c2vi.dev>
2024-09-03 08:57:35 +0200

    make comandr build for wasm

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,13 @@ pub use core::command;
 pub use core::hotkey;
 
 pub mod platform {
+    #[cfg(features = "qt")]
     pub mod qt;
     pub mod web;
 }
 
 pub mod ui {
+    #[cfg(features = "qt")]
     pub mod slint {
         slint::include_modules!();
     }
